6499e12d80f15b0b01a937b8c311ce735202395d
Vishal Bhardwaj <vishalbhardwaj872004@gmail.com>
2025-12-07 22:38:48 +0530

    add

--- server/server.js
+++ server/server.js
@@ -1,19 +1,19 @@
-
-require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const connectDB = require('./config/db')
-
 const authRoutes = require('./routes/authRoutes')
 
 const app = express()
 
+const allowedOrigin = process.env.CLIENT_ORIGIN || 'http://localhost:5173'
+
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: allowedOrigin,
     credentials: true
   })
 )
+
 app.use(express.json())
 
 app.use('/api/auth', authRoutes)
